7b4ca2c7f132aef6ca3b02e5542ed6c6ab4e3903
Ronit-gurjar <ronitgurjar999@gmail.com>
2026-03-29 15:44:47 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-900">
+      <h1 className="text-5xl font-black text-cyan-400 italic uppercase">
+        Shout or Loss: Ready to Race
+      </h1>
+    </div>
+  )
+}
+
+export default App
